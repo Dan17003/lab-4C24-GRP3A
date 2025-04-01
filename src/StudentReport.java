dce3785d9cd4dd2028fe8ed64c85baaf70d890cd
AxelLoayza <axtev2000@hotmail.com>
2025-04-01 18:01:51 -0500

    mi cambio

--- src/StudentReport.java
+++ src/StudentReport.java
@@ -1,2 +1,17 @@
-public class StudentReport {
+public abstract class StudentReport {
+    private String title;
+
+    public StudentReport(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public abstract void generateReport();
 }
